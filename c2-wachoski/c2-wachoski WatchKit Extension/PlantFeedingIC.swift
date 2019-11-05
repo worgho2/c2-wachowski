faac1827f94bdc442a9e231e9745a8f93bde023d
Otávio Baziewicz Filho <otavio.baziewicz.filho@gmail.com>
2019-11-05 17:45:20 -0300

    Fixing Names and adding files

--- c2-wachoski/c2-wachoski WatchKit Extension/PlantFeedingIC.swift
+++ c2-wachoski/c2-wachoski WatchKit Extension/PlantFeedingIC.swift
@@ -1,36 +1,30 @@
-//
-//  PlantFeeding.swift
-//  c2-wachoski WatchKit Extension
-//
-//  Created by João Raffs on 30/10/19.
-//  Copyright © 2019 Otávio Baziewicz Filho. All rights reserved.
-//
-
 import UIKit
 import Foundation
 import WatchKit
 
 class PlantFeeding: WKInterfaceController {
-    @IBOutlet weak var plantInterfaceImage: WKInterfaceImage!
     
+    @IBOutlet weak var scene: WKInterfaceSKScene!
     
     override func didAppear() {
-//        plantInterfaceImage.
-        
-//        print
+        super.didAppear()
         
         
     }
+    
     @IBAction func rightSwipeGestureRecognizer(_ sender: Any) {
         print("fertilizer")
     }
+    
     @IBAction func downSwipeGestureRecgnizer(_ sender: Any) {
         print("sun")
     }
+    
     @IBAction func leftSwipeGestureRecgnizer(_ sender: Any) {
         print("water")
     }
     
     
+
 }
     
