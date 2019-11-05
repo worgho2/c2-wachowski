61fd3337e5af4885729ca39fcc7e65535df24580
Otávio Baziewicz Filho <otavio.baziewicz.filho@gmail.com>
2019-11-05 17:45:58 -0300

    Remake Model, DataAccessObject and Interface

--- c2-wachoski/c2-wachoski WatchKit App/Model/DataAccessObject.swift
+++ c2-wachoski/c2-wachoski WatchKit App/Model/DataAccessObject.swift
@@ -1,79 +1,35 @@
-//
-//  DataAccessObject.swift
-//  c2-wachoski
-//
-//  Created by João Raffs on 23/10/19.
-//  Copyright © 2019 Otávio Baziewicz Filho. All rights reserved.
-//
-
 import Foundation
+
 class DataAccessObject {
-    public init(){}
-    static func loadFromUserDefaults() {
-        //pegar pra salvar no model
+    
+    private static let randomFilename = UUID().uuidString
+    private static let fullPath = getDocumentsDirectory().appendingPathComponent(randomFilename)
+    
+    static func getDocumentsDirectory() -> URL {
+        let paths = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
+        return paths[0]
+    }
+    
+    static func storeUser() {
+        do {
+            let data = try NSKeyedArchiver.archivedData(withRootObject: Model.instance.user!, requiringSecureCoding: false)
+            try data.write(to: fullPath)
+        } catch {
+            print("Couldn't write file")
+        }
+    }
+    
+    static func retrieveUser() -> User? {
+        guard let data = NSKeyedUnarchiver.unarchiveObject(withFile: fullPath.path) as? Data else { return nil }
         
-        self.loadPlantsFromUDIntoModel()
-        self.loadsUserFromUID()
-        Model.instance.user?.plants = Model.instance.plants
+        do {
+            if let loadedUser = try NSKeyedUnarchiver.unarchiveTopLevelObjectWithData(data) as? User {
+                return loadedUser
+            }
+        } catch {
+            print("Couldn't read file.")
+        }
         
-    }
-    
-    static func savePlantsFromModelOnUD(){
-        let plants = Model.instance.plants
-        var i = 0
-        while i < plants.count{
-            
-            UserDefaults.standard.set(plants[i].name , forKey: "plant\(i)name")
-            UserDefaults.standard.set(plants[i].id, forKey: "plant\(i)id")
-            UserDefaults.standard.set(plants[i].size, forKey: "plant\(i).size")
-            UserDefaults.standard.set(plants[i].size.water , forKey: "plant\(i).sizeWater")
-            UserDefaults.standard.set(plants[i].size.fertilizer , forKey: "plant\(i).sizeFertilizer")
-            UserDefaults.standard.set(plants[i].size.sun , forKey: "plant\(i).sizeSun")
-            i += 1
-            
-        }
-    }
-    
-    static func loadPlantsFromUDIntoModel(){
-        var i = 0
-        while true{
-            if let name = UserDefaults.standard.object(forKey: "plant\(i)name") as? String{
-                if let sizeWater = UserDefaults.standard.object(forKey: "plant\(i)sizeWater") as? String{
-                    if let id = UserDefaults.standard.object(forKey: "plant\(i)id") as? String{
-                        if let sizeFertilizer = UserDefaults.standard.object(forKey: "plant\(i)sizeWater") as? String{
-                            if let sizeSun = UserDefaults.standard.object(forKey: "plant\(i)sizeSuun") as? String{
-                                var plant = Plant(name: name, water: Int(sizeWater)!, sun:  Int(sizeFertilizer)!, fertilizer: Int(sizeFertilizer)! )
-                                plant.id = Int(id)!
-                                Model.instance.plants.append(plant)
-                            }
-                        }
-                    }
-                }
-            }
-            i += 1
-        }
-    }
-    
-    static func saveUserInUD(){
-        if let user = Model.instance.user{
-//            UserDefaults.standard.set(user.id, forKey "userID")
-            UserDefaults.standard.set(user.name, forKey: "userName")
-            UserDefaults.standard.set(user.id, forKey: "userID")
-        }
-    }
-    
-    static func loadsUserFromUID(){
-        if let userName = UserDefaults.standard.object(forKey: "userName") as? String{
-            if let id = UserDefaults.standard.object(forKey: "userID") as? Int{
-                let user = User(name: userName)
-                Model.instance.user?.id = id
-            }
-        }
-    }
-    
-    static func saveOnUserDefaults() {
-        savePlantsFromModelOnUD()
-        saveUserInUD()
-        //limpar tudo e escrever de novo ou verificar se tem mudanças
+        return nil
     }
 }
--- c2-wachoski/c2-wachoski WatchKit Extension/InterfaceController.swift
+++ c2-wachoski/c2-wachoski WatchKit Extension/InterfaceController.swift
@@ -1,66 +1,16 @@
-//
-//  InterfaceController.swift
-//  c2-wachoski WatchKit Extension
-//
-//  Created by Otávio Baziewicz Filho on 18/10/19.
-//  Copyright © 2019 Otávio Baziewicz Filho. All rights reserved.
-//
-
 import WatchKit
 import Foundation
 
-
-class InterfaceController: WKInterfaceController, WKCrownDelegate {
-    
-    //crown variables
-    var canResetZoom: Bool = false
-    var crownAccumulator = 0.0
-    var zoom : Int = 1
-
+class InterfaceController: WKInterfaceController {
     override func awake(withContext context: Any?) {
         super.awake(withContext: context)
-        crownSequencer.delegate = self
     }
     
     override func willActivate() {
         super.willActivate()
-        crownSequencer.focus()
     }
     
     override func didDeactivate() {
         super.didDeactivate()
     }
-    
-    func crownDidRotate(_ crownSequencer: WKCrownSequencer?, rotationalDelta: Double) {
-        self.canResetZoom = false
-        
-        crownAccumulator += rotationalDelta
-        
-        if abs(crownAccumulator) > 0.05 {
-            zoom = max(0, Int(sign(crownAccumulator)) + zoom)
-            let scale : CGFloat = CGFloat(abs(zoom)) * 0.1 + 1
-//            Desconmentar quando adicionar a scene
-//            self.scene.scene?.camera?.xScale = scale
-//            self.scene.scene?.camera?.yScale = scale
-            
-            crownAccumulator = 0
-        }
-        
-    }
-    
-    func crownDidBecomeIdle(_ crownSequencer: WKCrownSequencer?) {
-        self.canResetZoom = true
-        Timer.scheduledTimer(withTimeInterval: 3, repeats: false) { _ in
-            if self.canResetZoom {
-//                Desconmentar quando adicionar a scene
-//                self.scene.scene?.camera?.run(.scale(to: 1.0, duration: 0.3))
-                self.crownAccumulator = 0
-                self.zoom = 0
-            }
-        }
-    }
-
-//    @IBAction func botaoTeste() {
-//        print("botão")
-//    }
 }
