33d537f3231ee8269062f43409bb9e021cdf202a
Kelvin <kelvin.javorskisoares@gmail.com>
2019-11-06 16:27:12 -0300

    gitflow-feature-stash: HealthKit
    Healthkit Authorization, read Ring data

--- c2-wachoski/c2-wachoski WatchKit Extension/InterfaceController.swift
+++ c2-wachoski/c2-wachoski WatchKit Extension/InterfaceController.swift
@@ -8,7 +8,7 @@
 
 import WatchKit
 import Foundation
-
+import HealthKit
 
 class InterfaceController: WKInterfaceController, WKCrownDelegate {
     
@@ -16,10 +16,25 @@ class InterfaceController: WKInterfaceController, WKCrownDelegate {
     var canResetZoom: Bool = false
     var crownAccumulator = 0.0
     var zoom : Int = 1
+    var firstLoginDate = DateComponents()
+    var Energy = EnergyBurned()
+    var Stand = StandTime()
+    var Exercise = ExerciseTime()
+    var CurrentResources = Resources()
 
     override func awake(withContext context: Any?) {
         super.awake(withContext: context)
         crownSequencer.delegate = self
+        
+        //Verifica se o app tem autorização para utilizar os dados do HealthKit
+        if !HKHealthStore.isHealthDataAvailable(){
+            return
+        }
+        //Chama a função que pede autorização.
+        authorizeHealthKit()
+        firstLoginDate = setFirstLoginDate()
+        UpdateSumOfRingValues()
+        
     }
     
     override func willActivate() {
@@ -63,4 +78,56 @@ class InterfaceController: WKInterfaceController, WKCrownDelegate {
 //    @IBAction func botaoTeste() {
 //        print("botão")
 //    }
+    
+    //Autorização para o app utilizar os dados do dispositivo
+    private func authorizeHealthKit() {
+          HealthKitSetup.authorizeHealthKit { (authorized, error) in
+            guard authorized else {
+              let baseMessage = "HealthKit Authorization Failed"
+              if let error = error {
+                print("\(baseMessage). Reason: \(error.localizedDescription)")
+              } else {
+                print(baseMessage)
+              }
+              return
+            }
+            print("HealthKit Successfully Authorized.")
+          }
+        }
+    
+    
+    //Retorna a data do último login em formato DateComponents para
+    private func setFirstLoginDate() -> DateComponents{
+        var calendar = Calendar.autoupdatingCurrent
+        calendar.timeZone = TimeZone(identifier: "UTC")!
+
+        var todayComponents = calendar.dateComponents([.year, .month, .day], from: Date())
+        todayComponents.calendar = calendar
+        
+        return todayComponents
+    }
+    
+    private func UpdateSumOfRingValues(){
+        
+        //Retorna, para cada recurso, a soma de todos os dados da Ring do usuário, desde
+        //o ultimo login.
+        Energy = HealthKitData.getMostRecentEnergySample(initialDate: firstLoginDate)
+        Stand = HealthKitData.getMostRecentStandSample(initialDate: firstLoginDate)
+        Exercise = HealthKitData.getMostRecentExerciseSample(initialDate: firstLoginDate)
+        updateAccumulatedData()
+    }
+    
+    private func updateAccumulatedData(){
+        
+        //Atualiza os recursos disponível para o usuário utilizar
+        CurrentResources.energyBurned += Energy.currentValue - Energy.accumulated
+        CurrentResources.standTime += Stand.currentValue - Stand.accumulated
+        CurrentResources.exerciseTime += Exercise.currentValue - Exercise.accumulated
+        
+        //Registra a quantidade de recursos acumulado pelo usuário desde o primeiro login
+        //para manter a lógica top que eu usei.
+        Energy.accumulated = Energy.currentValue - Energy.accumulated
+        Stand.accumulated = Stand.currentValue - Stand.accumulated
+        Exercise.accumulated = Exercise.currentValue - Exercise.accumulated
+    }
 }
